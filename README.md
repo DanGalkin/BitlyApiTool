868ea47d33484068a1e860d2bd0e9ee956642b3c
DanGalkin <dangalkin@gmail.com>
2019-12-06 21:44:47 +0300

    Create the repo with initial files
    Initial files are:
    Main.py - file with a code
    README.md
    requirements.txt
    .env

---
+++ README.md
@@ -0,0 +1,33 @@
+# BitlyApiTool
+
+### Description
+
+BitlyApiTool is a program that takes a weblink as an argument and shortens it as a bitlink via API of Bitly.com. If the weblink provided as argument is already a bitlink, the statistics of it to be shown.
+
+### How to install
+
+BitlyApiTool needs an API token of your Bitly.com account. [The instruction to get it](https://support.bitly.com/hc/en-us/articles/230647907-How-do-I-find-my-OAuth-access-token-).
+
+Token information should be stored in the .env file located as shown below:
+```
+TOKEN=*your token*
+```
+Locate the .env file in the same folder with main.py.
+
+Python3 should be already installed. 
+Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Example
+
+```
+>python.exe main.py https://github.com/
+Это явно не ваш битлинк, попробуем сократить эту ссылку
+Сокращенная ссылка: http://bit.ly/362x8gS
+```
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import sys
+import os
+from dotenv import load_dotenv
+import argparse
+
+load_dotenv()
+
+API_ENDPOINT = 'https://api-ssl.bitly.com/v4/{method}'
+
+def create_parser():
+  parser = argparse.ArgumentParser(description='Программа сокращает ссылку в битлинк или выдает статистику по вашему битлинку')
+  parser.add_argument('link', help='Ваша ссылка')
+  return parser
+
+def make_oauth_headers(token):
+  oauth_header = 'Bearer {}'.format(token)
+  headers = {
+    'Authorization': oauth_header
+  }
+  return headers
+
+def shorten_link(long_url, headers):
+  data_to_shorten = {'long_url': long_url}
+  json_data_to_shorten = json.dumps(data_to_shorten)
+  short_link_response = requests.post(API_ENDPOINT.format(method='shorten'), headers=headers, data=json_data_to_shorten)
+  if short_link_response.ok:
+    return short_link_response.json()['link']
+
+def get_link_statistics(url, headers):
+  method = 'bitlinks/{}/clicks/summary'.format(url)
+  link_statistics_response = requests.get(API_ENDPOINT.format(method=method), headers=headers)
+  if link_statistics_response.ok:
+    return link_statistics_response.json()['total_clicks']
+
+def check_if_bitlink(url, headers):
+  method = 'bitlinks/{}'.format(url)
+  bitlink_response = requests.get(API_ENDPOINT.format(method=method), headers=headers)
+  return bitlink_response.ok
+
+def main():
+  token = os.getenv("TOKEN")
+  headers = make_oauth_headers(token)
+  bitly_user_response = requests.get(API_ENDPOINT.format(method='user'), headers=headers)
+  parser = create_parser()
+  link = parser.parse_args().link
+  if check_if_bitlink(link, headers):
+    print('Вы ввели битлинк, сейчас выгрузим статистику по нему')
+    link_statistics = get_link_statistics(link, headers)
+    if not link_statistics:
+      sys.exit('Что-то пошло не так. Программа завершит работу')
+    print('Статистика переходов по ссылке: {}'.format(link_statistics))
+  else:
+    print('Это явно не ваш битлинк, попробуем сократить эту ссылку')
+    bitly_link = shorten_link(link, headers)
+    if not bitly_link:
+      sys.exit('Bitly не может сократить ссылку, проверьте, что она правильная. Программа завершит работу')
+    print('Сокращенная ссылка: {}'.format(bitly_link))
+
+if __name__ == '__main__':
+  main()
